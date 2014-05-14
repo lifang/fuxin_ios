0fa909721f885f1680c904af6c9e8b931cd140a8
lhl0033 <lhl0033@gmail.com>
2014-05-14 10:53:50 +0800

    Initial Commit

---
+++ FuXin/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FuXin
+//
+//  Created by lihongliang on 14-5-14.
+//  Copyright (c) 2014年 comdosoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FuXin/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FuXin
+//
+//  Created by lihongliang on 14-5-14.
+//  Copyright (c) 2014年 comdosoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
