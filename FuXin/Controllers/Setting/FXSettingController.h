ad330b7bf3306f1d87f2497c27b5ddc4108a744d
XuBingoo <251311016@qq.com>
2014-06-03 13:16:39 +0800

    6.3_xbq

--- FuXin/Controllers/Setting/FXSettingController.h
+++ FuXin/Controllers/Setting/FXSettingController.h
@@ -2,12 +2,14 @@
 //  FXSettingController.h
 //  FuXin
 //
-//  Created by 徐宝桥 on 14-5-20.
+//  Created by 徐宝桥 on 14-6-2.
 //  Copyright (c) 2014年 ___MyCompanyName___. All rights reserved.
 //
 
 #import "FXAdjustViewController.h"
 
 @interface FXSettingController : FXAdjustViewController<UITableViewDataSource,UITableViewDelegate>
-@property (nonatomic,retain) UITableView *setTable;
+
+@property (nonatomic, strong) UITableView *settingTableView;
+
 @end
---
+++ FuXin/Views/FXSettingUserCell.h
@@ -0,0 +1,27 @@
+//
+//  FXSettingUserCell.h
+//  FuXin
+//
+//  Created by 徐宝桥 on 14-6-2.
+//  Copyright (c) 2014年 ___MyCompanyName___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef enum {
+    UserBtnNone = 0,
+    UserBtnInfo,
+    UserBtnMessage,
+    UserBtnPhone,
+}UserBtnTags;
+
+@interface FXSettingUserCell : UITableViewCell
+
+@property (nonatomic, strong) UIImageView *photoView;
+@property (nonatomic, strong) UILabel *nameLabel;
+@property (nonatomic, strong) UIImageView *sexView;
+@property (nonatomic, strong) UIButton *infoButton;
+@property (nonatomic, strong) UIButton *msgButton;
+@property (nonatomic, strong) UIButton *phoneButton;
+
+@end
